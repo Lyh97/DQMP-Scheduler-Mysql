799962e7171a935fa6044d0b377af68366cb0283
LawrenceDC <dengchenghh@yahoo.com>
2018-05-16 02:45:29 +0800

    taskname

--- SqlTask/SqlTask.py
+++ SqlTask/SqlTask.py
@@ -22,28 +22,19 @@ def run(user_id, taskid, filepath, freqency, threshold, taskname):
     result_count = int(result['1'])
     result_time = time.strftime('%Y-%m-%d', time.localtime())
     insert_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-    if freqency == 'daily':
-        daily_status = 1
-        query_db_outside(query['add_dailylog'], (taskid, result_count, result_time, 1, user_id, insert_time))
-
-    if freqency == 'weekly':
-        query_db_outside(query['add_dailylog'], (taskid, result_count, result_time, 1, user_id, insert_time))
-
-    if freqency == 'monthly':
-        query_db_outside(query['add_dailylog'], (taskid, result_count, result_time, 1, user_id, insert_time))
 
     table = {'daily': 'add_dailylog',
              'weekly': 'add_dailylog',
              'monthly': 'add_dailylog'}
 
     if result_count > threshold:
-        daily_status = 0
+        daily_status = 'Fail'
         resulttab_status = 'Fail'
         print('\033[1;33;41m<' + taskname + '> [id:' + taskid +']' + 'Exceed the threshold [count:'+ str(result_count) +']' + '\033[0m')
     else:
-        daily_status = 1
+        daily_status = 'Success'
         resulttab_status = 'Success'
         print('\033[1;30;47m<' + taskname + '> [id:' + taskid +']' + 'success!' + '\033[0m')
 
-    query_db_outside(query[table[freqency]], (taskid, result_count, result_time, daily_status, user_id, insert_time))
+    query_db_outside(query[table[freqency]], (taskid, result_count, result_time, daily_status, user_id, insert_time, taskname))
     query_db_outside(query['add_resulttab'], (taskname, taskid, result_count, resulttab_status, insert_time, '10s'))
